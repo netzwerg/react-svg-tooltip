03a8c6b265e148fe5c471b78703102bd8b09d559
Tim Coy <timothycoy@gmail.com>
2018-10-24 13:25:07 -0400

    Compile to es5

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import resolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import sourceMaps from 'rollup-plugin-sourcemaps'
 const pkg = require('./package.json')
@@ -10,7 +11,7 @@ export default {
   entry: `compiled/${libraryName}.js`,
   targets: [
     { dest: pkg.main, moduleName: camelCase(libraryName), format: 'umd' },
-	  { dest: pkg.module, format: 'es' }
+    { dest: pkg.module, format: 'es' }
   ],
   sourceMap: true,
   // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
@@ -22,6 +23,11 @@ export default {
     // which external modules to include in the bundle
     // https://github.com/rollup/rollup-plugin-node-resolve#usage
     resolve(),
+
+    babel({
+      exclude: 'node_modules/**',
+      presets: [['@babel/env', { modules: false }]],
+    }),
 
     // Resolve source maps to the original source
     sourceMaps()
